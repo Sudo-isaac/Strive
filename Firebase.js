12b99db06cc5cbaa9377359bea05fda4e8ff1a9d
Sudo-isaac <salzmannisaac64@gmail.com>
2026-05-15 11:16:42 +0800

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzK9Ke_4sz7eNLhhyNU3wvcq081talXCk",
@@ -15,5 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const db   = getFirestore(app);
